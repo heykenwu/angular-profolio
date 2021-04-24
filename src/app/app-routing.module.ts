0d925375b8b3bebaa05ca93174e6c495be2aa775
heykenwu <yanhwan@gmail.com>
2021-04-24 12:58:59 -0700

    mm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { MatSliderModule } from '@angular/material/slider';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),MatSliderModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
